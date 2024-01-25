3befcf70d8ef6416b994f6ea74ccf5a1ddca2061
Angel Mashego <thulisile.ma98@gmail.com>
2024-01-25 09:46:49 +0200

    exception

---
+++ src/main/java/codemasters/codematersspringcrud/exception/PostNotFoundException.java
@@ -0,0 +1,10 @@
+package codemasters.codematersspringcrud.exception;
+
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException(Long id) {
+        super("Could not find the user with id:" + id);
+    }
+
+    
+}
